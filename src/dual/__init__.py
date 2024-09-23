dfffc49cf96ed57a338a043564632e546c1d79a3
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2024-09-23 13:28:27 +0900

    upd

--- src/dual/__init__.py
+++ src/dual/__init__.py
@@ -1,3 +1,8 @@
+from importlib.metadata import metadata
+
 from .dual import dual, main
 
 __all__ = ["dual", "main"]
+_package_metadata = metadata(__package__)
+__version__ = _package_metadata["Version"]
+__author__ = _package_metadata.get("Author-email", "")
